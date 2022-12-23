ddb3c5b03da58b622f0c8c437ea03d58fe3f4dd6
TanuhaD <demchenkotanuha@gmail.com>
2022-12-23 16:15:56 +0200

    asd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,18 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Searchbar, ImageGallery } from './';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import { notify } from '../utils/notify';
 
-export class App extends Component {
-  state = {
-    query: '',
-  };
+const App = () => {
+  const [query, setQuery] = useState('');
 
-  setQuery = query => {
-    this.setState({ query });
-  };
-  notify = msg =>
-    toast(`🦄 ${msg}`, {
-      position: 'bottom-left',
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: 'light',
-    });
-
-  render() {
-    return (
-      <div className="app">
-        <Searchbar setQuery={this.setQuery} notifyFunc={this.notify} />
-        <ImageGallery query={this.state.query} notifyFunc={this.notify} />
-        <ToastContainer autoClose={3000} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app">
+      <Searchbar setQuery={setQuery} notifyFunc={notify} />
+      <ImageGallery query={query} notifyFunc={notify} />
+      <ToastContainer autoClose={3000} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,136 +1,99 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
-import fetchPictures from '../../utils/fetchAPI';
+import React, { useEffect, useState } from 'react';
 import { Circles } from 'react-loader-spinner';
-import Modal from '../Modal/Modal';
 import { IMAGES_PER_PAGE } from '../../utils/constants';
+import fetchPictures from '../../utils/fetchAPI';
+import Modal from '../Modal/Modal';
 import Button from './Button/Button';
-import throttle from 'lodash.throttle';
+import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 
-export class ImageGallery extends Component {
-  static propTypes = {
-    query: PropTypes.string.isRequired,
-    notifyFunc: PropTypes.func.isRequired,
-  };
+export const ImageGallery = ({ query, notifyFunc }) => {
+  const [imagesQuery, setImagesQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(1);
+  const [isModalShown, setIsModalShown] = useState(false);
+  const [modalImgUrl, setModalImgUrl] = useState('');
+  const [captionData, setCaptionData] = useState('');
 
-  state = {
-    images: [],
-    page: 1,
-    loading: false,
-    totalPages: 1,
-    isModalShown: false,
-    modalImgUrl: '',
-    captionData: '',
-  };
+  useEffect(() => {
+    setImagesQuery(query);
+    setPage(1);
+    setImages([]);
+  }, [query]);
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.throttledScrollHandler);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.throttledScrollHandler);
-  }
-
-  async componentDidUpdate(prevProps, prevState) {
-    if (this.props.query !== prevProps.query) {
-      this.setState({ loading: true });
-      const data = await fetchPictures(this.props.query, 1);
+  useEffect(() => {
+    if (!imagesQuery) return;
+    const fetchData = async () => {
+      setLoading(true);
+      const data = await fetchPictures(imagesQuery, page);
       if (!data.hits.length) {
-        this.props.notifyFunc('Nothing found for your request');
+        notifyFunc('Nothing found for your request');
       }
-      this.setState({
-        images: [...data.hits],
-        page: 1,
-        loading: false,
-        totalPages: Math.ceil(data.totalHits / IMAGES_PER_PAGE),
-      });
-    } else if (this.state.page !== prevState.page && this.state.page > 1) {
-      this.setState({ loading: true });
-      const data = await fetchPictures(this.props.query, this.state.page);
-      this.setState({
-        images: [...prevState.images, ...data.hits],
-        loading: false,
-      });
-    }
+      setImages(prev => [...prev, ...data.hits]);
+      setTotalPages(Math.ceil(data.totalHits / IMAGES_PER_PAGE));
+      setLoading(false);
+    };
+    fetchData();
+  }, [notifyFunc, page, imagesQuery]);
+
+  function nextPage() {
+    setPage(p => p + 1);
   }
 
-  handleScroll = () => {
-    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
-    if (
-      this.state.page < this.state.totalPages &&
-      scrollHeight - clientHeight - scrollTop < 200
-    ) {
-      this.nextPage();
-    }
+  const onImageClick = imageId => {
+    const imgInfo = images.find(({ id }) => id === imageId);
+    setIsModalShown(true);
+    setModalImgUrl(imgInfo.largeImageURL);
+    setCaptionData(imgInfo.tags);
   };
 
-  throttledScrollHandler = throttle(this.handleScroll.bind(this), 300);
+  return (
+    <>
+      {loading && (
+        <Circles
+          height="300"
+          width="300"
+          color="#4fa94d"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass="spinner"
+          visible={true}
+        />
+      )}
 
-  nextPage = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  onImageClick = imageId => {
-    const imgInfo = this.state.images.find(({ id }) => id === imageId);
-    this.setState({
-      isModalShown: true,
-      modalImgUrl: imgInfo.largeImageURL,
-      captionData: imgInfo.tags,
-    });
-  };
-
-  modalClose = () => {
-    this.setState({ isModalShown: false });
-  };
-
-  render() {
-    return (
-      <>
-        {this.state.loading && (
-          <Circles
-            height="300"
-            width="300"
-            color="#4fa94d"
-            ariaLabel="circles-loading"
-            wrapperStyle={{}}
-            wrapperClass="spinner"
-            visible={true}
+      <ul className="ImageGallery">
+        {images.map(image => (
+          <ImageGalleryItem
+            key={image.id}
+            id={image.id}
+            img={image.webformatURL}
+            onImageClick={onImageClick}
+            alt={image.tags}
           />
-        )}
-
-        <ul className="ImageGallery">
-          {this.state.images.map(image => (
-            <ImageGalleryItem
-              key={image.id}
-              id={image.id}
-              img={image.webformatURL}
-              onImageClick={this.onImageClick}
-              alt={image.tags}
-            />
-          ))}
-        </ul>
-        {this.state.images.length > 0 &&
-          this.state.page !== this.state.totalPages && (
-            <Button onClick={this.nextPage}>Load More</Button>
-          )}
-        {this.state.isModalShown && (
-          <Modal onClose={this.modalClose}>
-            <img
-              src={this.state.modalImgUrl}
-              alt={this.state.captionData}
-              onLoad={({ currentTarget }) => {
-                currentTarget.parentNode.classList.add('captionShow');
-              }}
-            />
-            <p className="CaptionData">{this.state.captionData}</p>
-          </Modal>
-        )}
-      </>
-    );
-  }
-}
-
+        ))}
+      </ul>
+      {images.length > 0 && page !== totalPages && (
+        <Button onClick={nextPage}>Load More</Button>
+      )}
+      {isModalShown && (
+        <Modal onClose={() => setIsModalShown(false)}>
+          <img
+            src={modalImgUrl}
+            alt={captionData}
+            onLoad={({ currentTarget }) => {
+              currentTarget.parentNode.classList.add('captionShow');
+            }}
+          />
+          <p className="CaptionData">{captionData}</p>
+        </Modal>
+      )}
+    </>
+  );
+};
+ImageGallery.propTypes = {
+  query: PropTypes.string.isRequired,
+  notifyFunc: PropTypes.func.isRequired,
+};
 export default ImageGallery;
